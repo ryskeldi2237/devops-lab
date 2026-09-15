9243ff7c5eb54d79242e204f1d3e03e85fdea4b0
Rys <ryskeldi2237@gmail.com>
2026-09-14 23:22:22 -0700

    fix broken part

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,7 @@ app.use(cors());
 
 app.get("/api/hello", (req, res) => {
   res.json({
-    message: "Hello from backend!",
-this is broken
+    message: "Hello from backend V2!",
     version: "v2",
   });
 });
